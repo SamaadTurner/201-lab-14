94e891bc15a70d48abed6bdbab45f9dddb3d925c
SamaadTurner <samaad.turner@gmail.com>
2023-07-27 14:59:19 -0700

    added some comments

--- js/chart.js
+++ js/chart.js
@@ -1,15 +1,19 @@
 'use strict';
-
+// getting elements for the chart from the id 'chart' in chart.html 
 let canvasElem = document.getElementById('chart')
 
 // TODO:
  // - Instantiate a new AppState
  let appState = new AppState();
- //- Use a method on that AppState to load vote data from localStorage.
+ //- Uses a method on that AppState to load vote data from localStorage.
  appState.loadItems();
 
- // - Create a data object for chart.js using your AppState's allProducts array.
+ // - Creates an object called 'data' for chart.js using the AppState's allProducts[]
  const data = {
+    // Go through allProducts[] and give each element the name product, and return the name of each product (product.name) in the new array.
+    // .map  method is an array method in JavaScript that iterates over each element of the array and creates a new array with the results of applying a provided function to each element.
+    // product => product.name: This part is an arrow function that takes each product from the appState.allProducts array as input and returns product.name. It extracts the name property from each product object.
+    // all in all, when this line of code is executed, it will create a new array containing only the names of each product from appState.allProducts. The resulting array will have the same number of elements as the original array, but each element will now be a product name.
     labels: appState.allProducts.map(product => product.name),
     datasets: [{
       label: 'Votes',
